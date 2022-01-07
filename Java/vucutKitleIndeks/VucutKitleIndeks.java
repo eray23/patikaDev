a61c3fba5a1139e83a33cee21c1c39e1217756af
eray23 <erayvural82@gmail.com>
2022-01-07 23:06:22 +0300

    08.01.2022 02.06

---
+++ Java/vucutKitleIndeks/VucutKitleIndeks.java
@@ -0,0 +1,19 @@
+package vucutKitleIndeks;
+
+import java.util.Scanner;
+
+public class VucutKitleIndeks {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("Boyunuzu metre cinsinden giriniz : ");
+		double boy = s.nextInt();
+		System.out.println("Kilonuzu giriniz : ");
+		double kilo = s.nextInt();
+		
+		double indeks = (kilo/boy*boy);
+		System.out.println("Vücut kitle indeksiniz : "+indeks);
+	}
+
+}
