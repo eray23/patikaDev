2fb459e79a31c904741bb15af36aa8ad55dba5a8
eray23 <erayvural82@gmail.com>
2022-01-13 15:41:34 +0300

    13.01.2022 15.41

---
+++ Java/yildiz/Yildiz.java
@@ -0,0 +1,29 @@
+package yildiz;
+
+public class Yildiz {
+
+	public static void main(String[] args) {
+		int n = 6;
+		
+		for(int i = 1; i<=n;i++) {
+			for(int k =1;k<=(n-i);k++) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=2*i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+		for(int i = n; i>0;i--) {
+			for(int k =1;k<=(n-i);k++) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=2*i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
